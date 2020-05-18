cd22ec79bea97390ffb688701093d75aef3066d9
Anna Popova <anna.popova201909@gmail.com>
2020-05-18 10:48:07 +0300

    added SqlUtils.java

--- src/test/java/ru/netology/pageObject/data/DataHelper.java
+++ src/test/java/ru/netology/pageObject/data/DataHelper.java
@@ -2,10 +2,6 @@ package ru.netology.pageObject.data;
 
 import com.github.javafaker.Faker;
 import lombok.Value;
-import lombok.val;
-
-import java.sql.DriverManager;
-import java.sql.SQLException;
 
 public class DataHelper {
 
@@ -30,42 +26,5 @@ public class DataHelper {
     @Value
     public static class VerificationCode {
         private String code;
-    }
-
-    public static String getVerificationCode() throws SQLException {
-        String userId = null;
-        val dataSQL = "SELECT id FROM users WHERE login = ?;";
-        try (
-                val conn = DriverManager.getConnection(
-                        "jdbc:mysql://127.0.0.1:3306/app", "app", "pass"
-                );
-                val idStmt = conn.prepareStatement(dataSQL);
-        ) {
-            idStmt.setString(1, "vasya");
-
-            try (val rs = idStmt.executeQuery()) {
-                if (rs.next()) {
-                    userId = rs.getString("id");
-                }
-            }
-        }
-
-        String code = null;
-        val authCode = "SELECT code FROM auth_codes WHERE user_id = ?;";
-        try (
-                val conn = DriverManager.getConnection(
-                        "jdbc:mysql://127.0.0.1:3306/app", "app", "pass"
-                );
-                val codeStmt = conn.prepareStatement(authCode);
-        ) {
-            codeStmt.setString(1, userId);
-
-            try (val rs = codeStmt.executeQuery()) {
-                if (rs.next()) {
-                    code = rs.getString("code");
-                }
-            }
-        }
-        return code;
     }
 }
--- src/test/java/ru/netology/pageObject/test/AppTest.java
+++ src/test/java/ru/netology/pageObject/test/AppTest.java
@@ -1,49 +1,51 @@
 package ru.netology.pageObject.test;
 
+import com.codeborne.selenide.Condition;
 import lombok.val;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import ru.netology.pageObject.data.DataHelper;
 import ru.netology.pageObject.page.LoginPage;
+import ru.netology.pageObject.sqlUtils.SqlUtils;
 
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AppTest {
+    SqlUtils mySql = new SqlUtils();
+
+ @BeforeAll
+         void setUp() {
+     open("http://localhost:9999");
+ }
+
     @Test
     void shouldCheckLogin() throws SQLException {
-        open("http://localhost:9999");
         val loginPage = new LoginPage();
         val authInfo = DataHelper.getValidAuthInfo();
         val verificationPage = loginPage.validLogin(authInfo);
-        val verificationCode = DataHelper.getVerificationCode();
-        val dashboardPage = verificationPage.verify(verificationCode);
-        $("h2[data-test-id=\"dashboard\"]").shouldBe(visible);
+        val verificationCode = SqlUtils.getVerificationCode();
+        verificationPage.verify(verificationCode);
+        $("h2[data-test-id=\"dashboard\"]").shouldBe(Condition.visible);
     }
 
     @Test
-    void shouldCheckIfBlocked() throws SQLException {
-        open("http://localhost:9999");
+    void shouldCheckIfBlocked() throws  SQLException {
         val loginPage = new LoginPage();
         val authInfo = DataHelper.getAuthInfoWithInvalidPassword();
         loginPage.validLogin(authInfo);
         loginPage.validLogin(authInfo);
         loginPage.validLogin(authInfo);
-        val statusSQL = "SELECT status FROM users WHERE login = ?;";
-        try (val conn = DriverManager.getConnection(
-                "jdbc:mysql://127.0.0.1:3306/app", "app", "pass");
-             val statusStmt = conn.prepareStatement(statusSQL);) {
-            statusStmt.setString(1, "vasya");
-            try (val rs = statusStmt.executeQuery()) {
-                if (rs.next()) {
-                    val status = rs.getString("status");
-                    assertTrue(statusSQL.equals("blocked"));
-                }
-            }
-        }
+        val statusSQL = mySql.getStatusFromDb();
+        assertEquals("blocked", statusSQL);
+    }
+
+    @AfterAll
+    void close() throws SQLException {
+     mySql.cleanDb();
     }
 }
